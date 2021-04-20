2c7a51f15c271c86c5babea07470b1c03869849d
Student User <student@workstation.lab.example.com>
2021-04-20 13:35:29 -0400

    vars

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,17 @@
 <?php
- print ($_ENV["MYMARIADB_SERVICE_HOST"]);
- print (':');
- print ($_ENV["MYMARIADB_SERVICE_PORT"]);
+ $servername = $_ENV["MYMARIADB_SERVICE_HOST"];
+ $serviceport = $_ENV["MYMARIADB_SERVICE_PORT"];
+ $username = "jtesar";
+ $password = "redhat";
+ $db = "db";
+
+
+ try {
+	   $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+	     // set the PDO error mode to exception
+	   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	   echo "Connected successfully";
+	   } catch(PDOException $e) {
+	     echo "Connection failed: " . $e->getMessage();
+	   }
 ?>
