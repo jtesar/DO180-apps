b7849f8fb02dd2221cad4e2b4bd5c4add1871175
Student User <student@workstation.lab.example.com>
2021-04-20 13:40:19 -0400

    vars

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -14,4 +14,17 @@
 	   } catch(PDOException $e) {
 	     echo "Connection failed: " . $e->getMessage();
 	   }
+
+ $sql = "SELECT * from names";
+ $result = $conn->query($sql);
+
+ if ($result->num_rows > 0) {
+// output data of each row
+   while($row = $result->fetch_assoc()) {
+      echo "name: " . $row["name"]."<br>";
+   }
+ } else {
+      echo "No names";
+   }
+   $conn->close();
 ?>
